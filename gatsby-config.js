7cf7c91ecda06ac17c9ff8afaa1e051725f823a4
Larry Gregory <larry.gregory@elastic.co>
2019-03-11 18:15:06 -0400

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,13 +53,14 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Larry Gregory`,
+        short_name: `Larry Gregory`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        include_favicon: true,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
