0a95b6c90921c3024633b1d03ba77e70566bf6ca
Larry Gregory <larry.gregory@elastic.co>
2019-03-12 12:28:10 -0400

    use hook

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,7 +5,7 @@
  * See: https://www.gatsbyjs.org/docs/static-query/
  */
 
-import { graphql, StaticQuery } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
@@ -14,43 +14,10 @@ import Header from "./header"
 import "./layout.scss"
 import Social from "./social";
 
-function renderFn(children) {
-  return (data) => (<>
-    <Helmet>
-      {
-        // @ts-ignore
-        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-6jHF7Z3XI3fF4XZixAuSu0gGKrXwoX/w3uFPxC56OtjChio7wtTGJWRW53Nhx6Ev" crossorigin="anonymous" />
-      }
-    </Helmet>
+const Layout = ({ children }) => {
 
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      className="section"
-    >
-      <div className="container">
-        <main>{children}</main>
-      </div>
-
-      <footer className="footer">
-        <div className="columns is-mobile">
-          <div className="column is-narrow">
-            <p>© {data.site.siteMetadata.author}</p>
-          </div>
-          <div className="column">
-            <div className="content is-pulled-right">
-              <Social />
-            </div>
-          </div>
-        </div>
-      </footer>
-
-    </div>
-  </>);
-}
-
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
+  const { site } = useStaticQuery(
+    graphql`
       query SiteTitleQuery {
         site {
           siteMetadata {
@@ -63,10 +30,41 @@ const Layout = ({ children }) => (
           }
         }
       }
-    `}
-    render={renderFn(children)}
-  />
-)
+    `
+  );
+
+  return (<>
+    <Helmet>
+      {
+        // @ts-ignore
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-6jHF7Z3XI3fF4XZixAuSu0gGKrXwoX/w3uFPxC56OtjChio7wtTGJWRW53Nhx6Ev" crossorigin="anonymous" />
+      }
+    </Helmet>
+
+    <Header siteTitle={site.siteMetadata.title} />
+    <div
+      className="section"
+    >
+      <div className="container">
+        <main>{children}</main>
+      </div>
+
+      <footer className="footer">
+        <div className="columns is-mobile">
+          <div className="column is-narrow">
+            <p>© {site.siteMetadata.author}</p>
+          </div>
+          <div className="column">
+            <div className="content is-pulled-right">
+              <Social />
+            </div>
+          </div>
+        </div>
+      </footer>
+
+    </div>
+  </>);
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
